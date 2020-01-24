b39a303b0b21a9cc7b89e973a3943e5e45dc3833
Genevieve Goebel <41485395+genevievegoebel@users.noreply.github.com>
2020-01-24 14:03:03 -0500

    Add files via upload

---
+++ Cuber Demo.R
@@ -0,0 +1,6 @@
+#This is dummy for the purpose of demonstrating how one might use GitHub for version control
+
+cuberprompt <- "Enter an integer to cube: "
+x <- as.integer(readline(cuberprompt)[[1]])
+
+y <- x^3
